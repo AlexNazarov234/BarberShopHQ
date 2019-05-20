357603f15eee751a00002ba16d0766b639b15e22
Alex Nazarov <pokupka91@mail.ru>
2019-05-20 12:14:28 +0300

    make colorpicker

--- app.rb
+++ app.rb
@@ -12,7 +12,29 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
-	@barbers = Barber.order "created_at desc"
 	erb :index
+end
+
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+
+
+	erb "<h2>Спасибо, Вы записались!</h2>"
+
 end
